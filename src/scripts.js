d34e35f30c42041e918f718f9d7b9d031933ce81
Georg Grossberger <kontakt@grossberger-ge.org>
2014-12-23 15:42:52 +0100

    Use hash based routing, not click - listeners

--- src/scripts.js
+++ src/scripts.js
@@ -5,66 +5,81 @@
  * It is free software; you can redistribute it and/or modify it under *
  * the terms of the MIT aka X11 license                                *
  *                                                                     */
+(function(win, doc) {
 
+	var
+		boxes = [],
+		pollTimeout,
+		baseUrl = "boxserver_url";
 
-var
-	boxes = [],
-	pollTimeout,
-	baseUrl = "boxserver_url";
+	function pollBoxes() {
+		if (pollTimeout) {
+			clearTimeout(pollTimeout);
+		}
 
-function pollBoxes() {
-	if (pollTimeout) {
-		clearTimeout(pollTimeout);
-	}
+		boxes = [];
 
-	boxes = [];
+		var template = doc.querySelector('#BoxItem');
 
-	var template = document.querySelector('#BoxItem');
-
-	xhrGet("", function(projects) {
-		projects.forEach(function(project) {
-			xhrGet(project, function(boxnames) {
-				boxnames.forEach(function(box) {
-					boxes.push(project + "/" + box);
+		xhrGet("", function(projects) {
+			projects.forEach(function(project) {
+				xhrGet(project, function(boxnames) {
+					boxnames.forEach(function(box) {
+						boxes.push(project + "/" + box);
+					});
+					console.log(boxes)
 				});
-				console.log(boxes)
 			});
 		});
+
+		pollTimeout = setTimeout(pollBoxes, 5000);
+	}
+
+	function xhrGet(res, cb) {
+		var
+			xhr = new XMLHttpRequest(),
+			onload = function() {
+				try {
+					var data = JSON.parse(xhr.responseText);
+					cb(null, data);
+
+				} catch (err) {
+					cb(err, null)
+				}
+				cleanup();
+			},
+			onerror = function() {
+				console.error(xhr);
+				try {
+					var data = JSON.parse(xhr.responseText);
+					cb(new Error("Invalid request", xhr.status), data)
+				} catch (err) {
+					cb(err, null)
+				}
+				cleanup();
+			},
+			cleanup = function() {
+				xhr.removeEventListener("load", onload);
+				xhr.removeEventListener("error", onerror);
+				xhr = null;
+			};
+
+		xhr.addEventListener("load", onload);
+		xhr.addEventListener("error", onerror);
+		xhr.open("GET", baseUrl + res, true);
+		xhr.send(null)
+	}
+
+	function setDocumentByHash() {
+		var hash = ("" + location.hash).replace(/\#+/, "").split("/");
+	}
+
+	doc.addEventListener("DOMContentLoaded", function() {
+		pollBoxes();
 	});
 
-	pollTimeout = setTimeout(pollBoxes, 5000);
-}
+	win.addEventListener("hashchange", function() {
 
-function xhrGet(res, cb) {
-	var
-		xhr = new XMLHttpRequest(),
-		onload = function() {
-			try {
-				var data = JSON.parse(xhr.responseText);
-				cb(null, data);
+	});
 
-			} catch (err) {
-				cb(err, null)
-			}
-			cleanup();
-		},
-		onerror = function() {
-			console.error(xhr);
-			cb(new Error("Invalid request", xhr.status), null)
-			cleanup();
-		},
-		cleanup = function() {
-			xhr.removeEventListener("load", onload);
-			xhr.removeEventListener("error", onerror);
-			xhr = null;
-		};
-
-	xhr.addEventListener("load", onload);
-	xhr.addEventListener("error", onerror);
-	xhr.open("GET", baseUrl + res, true);
-	xhr.send(null)
-}
-
-document.addEventListener("DOMContentLoaded", function() {
-	pollBoxes();
-});
+})(window, document);
